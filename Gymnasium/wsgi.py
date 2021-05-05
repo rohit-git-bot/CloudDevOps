96e944f2c2e15c6d17435ca3d97433784f57fae7
EC2 Default User <ec2-user@ip-172-31-52-210.ec2.internal>
2021-05-05 07:45:32 +0000

    adding environment variable

--- Gymnasium/wsgi.py
+++ Gymnasium/wsgi.py
@@ -12,5 +12,5 @@ from notifications.config import get_notification_count, run_notifier
 from django.core.wsgi import get_wsgi_application
 run_notifier()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Gymnasium.settings")
-
+os.environ["DJANGO_SETTINGS_MODULE"] = "Gymnasium.settings"
 application = get_wsgi_application()
